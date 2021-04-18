fd615aace16b0209296c11d225c0bb3ae51d3d80
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2021-04-18 21:14:53 +0545

    Add files via upload

---
+++ tweet.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SankizTime - Tweet</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/tweet.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+	<!-- Add icon library -->
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+	<center>
+		<div class="main">
+			<input type="text" class="textbox" placeholder="What's happening?"><br><hr>
+			<li><i class="far fa-file-image"></i></li>
+			<li><i class="fas fa-map-marker-alt"></i></li>
+        	<li><i class="far fa-grin"></i></li>
+        	<li><i class="far fa-user"></i></li>
+			<button>Tweet</button>
+		</div>
+	</center>
+</body>
+</html>
